16243830a4e86381d2e4d1d7e8a9c58c5efa660f
Frank Boës <fboes@3960.org>
2023-03-13 16:25:47 +0100

    Improved web component performance

--- dist/Web/Components.js
+++ dist/Web/Components.js
@@ -5,6 +5,25 @@ import { SkyVector } from "../Export/SkyVector.js";
 import { LonLatArea } from "../World/LonLat.js";
 import { LonLatDate } from "../World/LonLatDate.js";
 class ComponentsOutputtable extends HTMLElement {
+    constructor() {
+        super();
+        this.elements = {
+            table: document.createElement("table"),
+            caption: document.createElement("caption"),
+            thead: document.createElement("thead"),
+            tbody: document.createElement("tbody"),
+        };
+        this.elements.table.appendChild(this.elements.caption);
+        this.elements.table.appendChild(this.elements.thead);
+        this.elements.table.appendChild(this.elements.tbody);
+        this.appendChild(this.elements.table);
+    }
+    draw() {
+        this.elements.table.classList.toggle("empty", !this.mission);
+        if (!this.mission) {
+            this.elements.tbody.innerHTML = "";
+        }
+    }
     /**
      * @param fields Table cell contents
      * @param join `td` or `th`; to supress a `th` at the beginnining of a `tr`
@@ -91,20 +110,17 @@ class ComponentsOutputtable extends HTMLElement {
 export class ComponentsWeather extends ComponentsOutputtable {
     constructor() {
         super();
+        this.elements.caption.innerText = "Weather";
+        this.elements.thead.innerHTML = this.outputLine(["Wind ", "Clouds", "Visibility", "Min flight rules"], "th");
         this.draw();
     }
     draw() {
+        super.draw();
         const m = this.mission;
         if (!m) {
-            this.innerHTML = "";
             return;
         }
         let html = "";
-        html += `<table>
-    <caption>Weather</caption>
-    <thead>`;
-        html += this.outputLine(["Wind ", "Clouds", "Visibility", "Min flight rules"], "th");
-        html += "</thead><tbody>";
         html += this.outputLine([
             Quote.html(this.getWind(m.conditions)) + "&nbsp;kts",
             this.outputCover(m.conditions.cloud) +
@@ -119,19 +135,20 @@ export class ComponentsWeather extends ComponentsOutputtable {
                 "&nbsp;SM",
             m.conditions.getFlightCategory(m.origin_country !== "US"),
         ], "ttd");
-        html += "</tbody></table>";
-        this.innerHTML = html;
+        this.elements.tbody.innerHTML = html;
     }
 }
 export class ComponentsAirports extends ComponentsOutputtable {
     constructor() {
         super();
+        this.elements.caption.innerText = "Airports";
+        this.elements.thead.innerHTML = this.outputLine(["Type", "Location ", "Country", "Date &amp; time ", '<abbr title="Local solar time">LST</abbr>', " Sun"], "th");
         this.draw();
     }
     draw() {
+        super.draw();
         const m = this.mission;
         if (!m) {
-            this.innerHTML = "";
             return;
         }
         const total_time_enroute = m.time_enroute;
@@ -140,11 +157,6 @@ export class ComponentsAirports extends ComponentsOutputtable {
         time.setSeconds(time.getSeconds() + total_time_enroute * 3600);
         const sunStateDestination = new LonLatDate(m.destination_lon_lat, time).sunState;
         let html = "";
-        html += `<table>
-    <caption>Airports</caption>
-    <thead>`;
-        html += this.outputLine(["Type", "Location ", "Country", "Date &amp; time ", '<abbr title="Local solar time">LST</abbr>', " Sun"], "th");
-        html += "</thead><tbody>";
         html += this.outputLine([
             "Departure",
             `<a target="skyvector" href="https://skyvector.com/airport/${encodeURIComponent(m.origin_icao)}">${Quote.html(m.origin_icao)}</a>`,
@@ -161,33 +173,18 @@ export class ComponentsAirports extends ComponentsOutputtable {
             sunStateDestination.localSolarTime,
             this.outputSunState(sunStateDestination),
         ]);
-        html += "</tbody></table>";
-        this.innerHTML = html;
+        this.elements.tbody.innerHTML = html;
     }
 }
 export class ComponentsCheckpoints extends ComponentsOutputtable {
     constructor() {
         super();
-        this.draw();
-    }
-    draw() {
-        const m = this.mission;
-        if (!m || !this.mission) {
-            this.innerHTML = "";
-            return;
-        }
-        const s = new SkyVector(m);
-        const lonLatArea = new LonLatArea(this.mission.origin_lon_lat);
-        this.mission.checkpoints.forEach((c) => {
-            lonLatArea.push(c.lon_lat);
-        });
-        const zoomLevel = lonLatArea.getZoomLevel();
-        const center = lonLatArea.center;
-        let html = "";
-        html += `<table>
-    <caption>Checkpoints</caption>
-    <thead>`;
-        html += this.outputLine([
+        this.elementsTfoot = document.createElement("tfoot");
+        this.elementsP = document.createElement("p");
+        this.elements.table.appendChild(this.elementsTfoot);
+        this.appendChild(this.elementsP);
+        this.elements.caption.innerText = "Checkpoints";
+        this.elements.thead.innerHTML = this.outputLine([
             "#",
             "Waypoint ",
             '<abbr title="Frequency">FRQ</abbr>',
@@ -198,52 +195,78 @@ export class ComponentsCheckpoints extends ComponentsOutputtable {
             "Distance",
             '<abbr title="Estimated time enroute">ETE</abbr>',
         ], "th");
-        html += "</thead><tbody>";
-        m.checkpoints.forEach((c, i) => {
-            const isAirport = c.type === MissionCheckpoint.TYPE_ORIGIN ||
-                c.type === MissionCheckpoint.TYPE_DESTINATION ||
-                i == 0 ||
-                i == m.checkpoints.length - 1;
-            const isAirportOrRunway = isAirport ||
-                c.type === MissionCheckpoint.TYPE_DEPARTURE_RUNWAY ||
-                c.type === MissionCheckpoint.TYPE_DESTINATION_RUNWAY;
-            html += this.outputLine([
-                Outputtable.pad(i + 1, 2, 0, "0") + ".",
-                !isAirport
-                    ? `<input title="Waypoint #${i + 1}" data-cp-id="${i}" data-cp-prop="name" type="text" value="${c.name}" pattern="[A-Z0-9._+-]+" maxlength="8" autocapitalize="characters" required="required" />`
-                    : c.name,
-                `<input title="Frequency #${i + 1}" data-cp-id="${i}" data-cp-prop="frequency_mhz" type="number" min="0.190" step="0.001" max="118" value="${c.frequency ? c.frequency_mhz : ""}" />&nbsp;MHz`,
-                `<input title="Altitude #${i + 1}" data-cp-id="${i}" data-cp-prop="altitude_ft" type="number" min="${!isAirportOrRunway ? -1000 : 0}" step="${!isAirportOrRunway ? 100 : 1}" value="${c.lon_lat.altitude_m ? Math.round(c.lon_lat.altitude_ft) : ""}" />&nbsp;ft`,
-                i !== 0
-                    ? `<input title="True Air Speed #${i + 1}" data-cp-id="${i}" data-cp-prop="speed" type="number" min="0" value="${c.speed >= 0 ? Math.round(c.speed) : ""}" />&nbsp;kts`
-                    : "",
-                i !== 0 ? Outputtable.padThree(c.direction_magnetic) + "°" : "",
-                i !== 0 ? '<span class="heading">' + Outputtable.padThree(c.heading_magnetic) + "</span>°" : "",
-                i !== 0
-                    ? `<span class="distance" title="${c.slope_deg.toFixed(1)}°">${Outputtable.pad(c.distance, 5, 1)}</span>&nbsp;NM`
-                    : "",
-                i !== 0
-                    ? '<span class="time_enroute">' + Outputtable.convertHoursToMinutesString(c.time_enroute) + "</span>"
-                    : "",
-            ]);
+        this.draw();
+    }
+    draw() {
+        super.draw();
+        const m = this.mission;
+        if (!m || !this.mission || m.checkpoints.length === 0) {
+            this.elements.table.classList.add("empty");
+            this.elementsTfoot.innerHTML = "";
+            this.elementsP.innerHTML = "";
+            return;
+        }
+        const s = new SkyVector(m);
+        const lonLatArea = new LonLatArea(this.mission.origin_lon_lat);
+        this.mission.checkpoints.forEach((c) => {
+            lonLatArea.push(c.lon_lat);
         });
-        html += "</tbody><tfoot>";
-        html += this.outputLine([
-            "",
-            "Total",
-            "",
-            "",
-            "",
-            "",
-            "",
-            Outputtable.pad(m.distance, 4, 1) + "&nbsp;NM",
-            '<span class="time_enroute">' + Outputtable.convertHoursToMinutesString(m.time_enroute) + "</span>",
-        ]);
-        html += "</tfoot></table>";
-        html += `<p class="no-print">Check your <a href="${s.toString(false)}" target="skyvector" title="${s
-            .getCheckpoints(false)
-            .join(" ")}">current flight plan on Sky Vector</a>.
+        const zoomLevel = lonLatArea.getZoomLevel();
+        const center = lonLatArea.center;
+        {
+            let html = "";
+            m.checkpoints.forEach((c, i) => {
+                const isAirport = c.type === MissionCheckpoint.TYPE_ORIGIN ||
+                    c.type === MissionCheckpoint.TYPE_DESTINATION ||
+                    i == 0 ||
+                    i == m.checkpoints.length - 1;
+                const isAirportOrRunway = isAirport ||
+                    c.type === MissionCheckpoint.TYPE_DEPARTURE_RUNWAY ||
+                    c.type === MissionCheckpoint.TYPE_DESTINATION_RUNWAY;
+                html += this.outputLine([
+                    Outputtable.pad(i + 1, 2, 0, "0") + ".",
+                    !isAirport
+                        ? `<input title="Waypoint #${i + 1}" data-cp-id="${i}" data-cp-prop="name" type="text" value="${c.name}" pattern="[A-Z0-9._+-]+" maxlength="8" autocapitalize="characters" required="required" />`
+                        : c.name,
+                    `<input title="Frequency #${i + 1}" data-cp-id="${i}" data-cp-prop="frequency_mhz" type="number" min="0.190" step="0.001" max="118" value="${c.frequency ? c.frequency_mhz : ""}" />&nbsp;MHz`,
+                    `<input title="Altitude #${i + 1}" data-cp-id="${i}" data-cp-prop="altitude_ft" type="number" min="${!isAirportOrRunway ? -1000 : 0}" step="${!isAirportOrRunway ? 100 : 1}" value="${c.lon_lat.altitude_m ? Math.round(c.lon_lat.altitude_ft) : ""}" />&nbsp;ft`,
+                    i !== 0
+                        ? `<input title="True Air Speed #${i + 1}" data-cp-id="${i}" data-cp-prop="speed" type="number" min="0" value="${c.speed >= 0 ? Math.round(c.speed) : ""}" />&nbsp;kts`
+                        : "",
+                    i !== 0 ? Outputtable.padThree(c.direction_magnetic) + "°" : "",
+                    i !== 0 ? '<span class="heading">' + Outputtable.padThree(c.heading_magnetic) + "</span>°" : "",
+                    i !== 0
+                        ? `<span class="distance" title="${c.slope_deg.toFixed(1)}°">${Outputtable.pad(c.distance, 5, 1)}</span>&nbsp;NM`
+                        : "",
+                    i !== 0
+                        ? '<span class="time_enroute">' + Outputtable.convertHoursToMinutesString(c.time_enroute) + "</span>"
+                        : "",
+                ]);
+            });
+            this.elements.tbody.innerHTML = html;
+        }
+        {
+            let html = "";
+            html += this.outputLine([
+                "",
+                "Total",
+                "",
+                "",
+                "",
+                "",
+                "",
+                Outputtable.pad(m.distance, 4, 1) + "&nbsp;NM",
+                '<span class="time_enroute">' + Outputtable.convertHoursToMinutesString(m.time_enroute) + "</span>",
+            ]);
+            this.elementsTfoot.innerHTML = html;
+        }
+        {
+            let html = "";
+            html += `<p class="no-print">Check your <a href="${s.toString(false)}" target="skyvector" title="${s
+                .getCheckpoints(false)
+                .join(" ")}">current flight plan on Sky Vector</a>.
     You may also want to take a look at <a href="https://www.google.com/maps/@?api=1&amp;map_action=map&amp;center=${encodeURIComponent(center.lat)},${encodeURIComponent(center.lon)}&amp;zoom=${encodeURIComponent(zoomLevel)}&amp;basemap=terrain" target="gmap">Google Maps</a> / <a href="https://www.openstreetmap.org/#map=${encodeURIComponent(zoomLevel)}/${encodeURIComponent(center.lat)}/${encodeURIComponent(center.lon)}" target="osm">OpenStreetMap</a>.</p>`;
-        this.innerHTML = html;
+            this.elementsP.innerHTML = html;
+        }
     }
 }
